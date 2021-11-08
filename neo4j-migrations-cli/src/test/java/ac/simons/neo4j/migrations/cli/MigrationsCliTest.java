b6529c996423b013b450ad48b84028efc3a76d48
Michael Simons <michael@simons.ac>
2021-11-08 10:01:04 +0100

    Fix flaky test.

--- neo4j-migrations-cli/src/test/java/ac/simons/neo4j/migrations/cli/MigrationsCliTest.java
+++ neo4j-migrations-cli/src/test/java/ac/simons/neo4j/migrations/cli/MigrationsCliTest.java
@@ -21,43 +21,18 @@ import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 import ac.simons.neo4j.migrations.core.Migrations;
 import picocli.CommandLine;
 
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
-import java.io.UnsupportedEncodingException;
 import java.lang.reflect.Field;
-import java.nio.charset.StandardCharsets;
 
 import org.graalvm.nativeimage.ImageInfo;
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.platform.commons.util.ReflectionUtils;
 
 /**
  * @author Michael J. Simons
  */
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class MigrationsCliTest {
 
 	@Test
-	@Order(1) // Should run first, as otherwise the outputstream cannot be set
-	void shouldLogToSysOut() throws UnsupportedEncodingException {
-		PrintStream originalSysout = System.out;
-		final ByteArrayOutputStream baos = new ByteArrayOutputStream();
-
-		try (PrintStream ps = new PrintStream(baos, true, StandardCharsets.UTF_8.name())) {
-			System.setOut(ps);
-			MigrationsCli.LOGGER.info("Test");
-			String result = new String(baos.toByteArray(), StandardCharsets.UTF_8);
-			assertThat(result).isEqualTo("Test" + System.lineSeparator());
-		} finally {
-			System.setOut(originalSysout);
-		}
-	}
-
-	@Test
-	@Order(2)
 	void shouldFailToScanPackageInNative() {
 
 		runTestPretendingToBeInNativeImage(() -> {
@@ -96,7 +71,6 @@ class MigrationsCliTest {
 	}
 
 	@Test
-	@Order(3)
 	void shouldNotFailToScanPackageInJVM() throws IllegalAccessException {
 
 		MigrationsCli cli = new MigrationsCli();
@@ -109,7 +83,6 @@ class MigrationsCliTest {
 	}
 
 	@Test
-	@Order(4)
 	void shouldHandleUnsupportedConfigException() throws IllegalAccessException {
 
 		MigrationsCli cli = new MigrationsCli();

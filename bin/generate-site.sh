befea0ea9a159cc336151cec072db087bae34251
Michael Simons <michael@simons.ac>
2021-12-14 18:21:31 +0100

    docs: Create a dedicated manual instead of one overly long readme. (#339)

---
+++ bin/generate-site.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+DIR="$(dirname "$(realpath "$0")")"
+
+cd $DIR/..
+./mvnw -B -q clean
+# For whatever reasons  maven-project-info-reports-plugin:3.1.2:dependencies goes nuts on that one
+# it's a pom project, there will be no classes, but apparently the plugin thinks different
+mkdir -p neo4j-migrations-spring-boot-starter-parent/neo4j-migrations-spring-boot-starter/target/classes
+./mvnw -B -q compile site:site site:stage
---
+++ neo4j-migrations-core/src/main/java/ac/simons/neo4j/migrations/core/utils/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Internal, utility classes. Not to be used outside the Core API. They won't be accessible when this project is used
+ * on the Java Module Path.
+ */
+package ac.simons.neo4j.migrations.core.utils;

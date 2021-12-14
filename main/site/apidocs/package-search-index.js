731ca06c0c24529ed2cbee4cc185b1ecb37b2d0d
Michael Simons <michael@simons.ac>
2021-12-14 16:11:38 +0100

    Add manually created main version.

---
+++ main/site/apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"ac.simons.neo4j.migrations.cli"},{"l":"ac.simons.neo4j.migrations.cli.internal"},{"l":"ac.simons.neo4j.migrations.core"},{"l":"ac.simons.neo4j.migrations.core.utils"},{"l":"ac.simons.neo4j.migrations.maven"},{"l":"ac.simons.neo4j.migrations.springframework.boot.autoconfigure"},{"l":"All Packages","u":"allpackages-index.html"}];updateSearchResults();

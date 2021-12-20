9ae85a5b0285c8b7f559936f1f4883b2d908c751
GitHub Action <action@github.com>
2021-12-20 08:43:32 +0000

    Update GH-Pages.

---
+++ 1.2.2/site/neo4j-migrations-quarkus-parent/neo4j-migrations-quarkus-deployment/apidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"ac.simons.neo4j.migrations.quarkus.deployment","l":"MigrationsBuildItem"},{"p":"ac.simons.neo4j.migrations.quarkus.deployment","l":"MigrationsProcessor$$accessor"}];updateSearchResults();

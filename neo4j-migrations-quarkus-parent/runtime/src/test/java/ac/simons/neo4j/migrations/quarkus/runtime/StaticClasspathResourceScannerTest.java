01bfc3ebd45ca47852fd58ee696f9f906efa6430
Michael J. Simons <michael@simons.ac>
2022-05-07 13:14:38 +0200

    fix: Add a test for the GraalVM 22.1 fix.
    I hate the red quality gate and I figure it's easier to add a test than
    figuring out how to exclude that line from Sonar. But: Digging into how
    to add a custom URL handler makes me reconsider my lifechoices once again.

--- neo4j-migrations-quarkus-parent/runtime/src/test/java/ac/simons/neo4j/migrations/quarkus/runtime/StaticClasspathResourceScannerTest.java
+++ neo4j-migrations-quarkus-parent/runtime/src/test/java/ac/simons/neo4j/migrations/quarkus/runtime/StaticClasspathResourceScannerTest.java
@@ -15,18 +15,71 @@
  */
 package ac.simons.neo4j.migrations.quarkus.runtime;
 
+import static com.github.stefanbirkner.systemlambda.SystemLambda.restoreSystemProperties;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
+import ac.simons.neo4j.migrations.core.MigrationsException;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
 import java.util.Set;
 
+import org.graalvm.nativeimage.ImageInfo;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 
 /**
+ * Order is necessary as the handler doesn't want to go away after first discovery.
+ *
  * @author Michael J. Simons
  */
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class StaticClasspathResourceScannerTest {
 
 	@Test
+	@Order(10)
+	void shouldDetectResourcesInNativeImageWithoutHandler() throws Exception {
+
+		var scanner = new StaticClasspathResourceScanner();
+		var resource = new ResourceWrapper();
+		var locations = List.of("/foo");
+		resource.setUrl("file:///foo/bar.cypher");
+		resource.setPath("foo");
+		scanner.setResources(Set.of(resource));
+
+		restoreSystemProperties(() -> {
+			System.setProperty(ImageInfo.PROPERTY_IMAGE_CODE_KEY, ImageInfo.PROPERTY_IMAGE_CODE_VALUE_RUNTIME);
+			assertThatExceptionOfType(MigrationsException.class)
+				.isThrownBy(() -> scanner.scan(locations))
+				.withCauseInstanceOf(MalformedURLException.class)
+				.withStackTraceContaining("unknown protocol: resource");
+		});
+	}
+
+	@Test
+	@Order(20)
+	void shouldDetectResourcesInNativeImage() throws Exception {
+
+		var scanner = new StaticClasspathResourceScanner();
+		var resource = new ResourceWrapper();
+		resource.setUrl("file:///foo/bar.cypher");
+		resource.setPath("foo");
+		scanner.setResources(Set.of(resource));
+
+		restoreSystemProperties(() -> {
+			System.setProperty("java.protocol.handler.pkgs", "ac.simons.neo4j.migrations.quarkus.runtime.protocol");
+			System.setProperty(ImageInfo.PROPERTY_IMAGE_CODE_KEY, ImageInfo.PROPERTY_IMAGE_CODE_VALUE_RUNTIME);
+			List<URL> urls = scanner.scan(List.of("/foo"));
+			assertThat(urls).map(URL::toString).containsExactly("resource:/foo");
+		});
+	}
+
+	@Test
+	@Order(30)
 	void shouldStoreResources() {
 
 		var resource = new ResourceWrapper();

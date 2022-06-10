0d552876d85e4bd55fee2e702a5aedbe715a7d63
Michael Simons <michael@simons.ac>
2022-06-10 12:51:29 +0200

    build: Optionally use a local database for Windows tests. (#523)

---
+++ bin/check_availability.java
@@ -0,0 +1,34 @@
+///usr/bin/env jbang "$0" "$@" ; exit $?
+//JAVA 17
+//DEPS org.neo4j.driver:neo4j-java-driver:4.4.6
+import java.time.Duration;
+import java.util.Objects;
+
+import org.neo4j.driver.AuthTokens;
+import org.neo4j.driver.GraphDatabase;
+import org.neo4j.driver.exceptions.Neo4jException;
+
+public class check_availability {
+
+	public static void main(String... args) throws InterruptedException {
+
+		if (args.length < 2) {
+			throw new IllegalArgumentException("Required arguments are <NAME_OF_URL_VAR> <NAME_OF_PWD_VAR>");
+		}
+
+		if (args.length == 3) {
+			Thread.sleep(Duration.ofSeconds(Integer.parseInt(args[2])).toMillis());
+		}
+
+		var url = Objects.requireNonNull(System.getenv(args[0]));
+		var pwd = Objects.requireNonNull(System.getenv(args[1]));
+		try (var driver = GraphDatabase.driver(url, AuthTokens.basic("neo4j", pwd))) {
+			driver.verifyConnectivity();
+			System.out.println("Database available.");
+		} catch (Neo4jException e) {
+			System.err.println(e.getMessage());
+			System.exit(1);
+		}
+		System.exit(0);
+	}
+}
---
+++ bin/start_neo4j.java
@@ -0,0 +1,23 @@
+///usr/bin/env jbang "$0" "$@" ; exit $?
+//JAVA 17
+//JAVA_OPTIONS --add-opens java.base/java.lang=ALL-UNNAMED --add-opens java.base/java.nio=ALL-UNNAMED
+//DEPS org.neo4j:neo4j:4.4.6
+//DEPS org.neo4j:neo4j-bolt:4.4.6
+import java.io.IOException;
+import java.nio.file.Files;
+
+import org.neo4j.configuration.connectors.BoltConnector;
+import org.neo4j.configuration.helpers.SocketAddress;
+import org.neo4j.dbms.api.DatabaseManagementServiceBuilder;
+
+public class start_neo4j {
+
+	public static void main(String... args) throws IOException {
+
+		new DatabaseManagementServiceBuilder(Files.createTempDirectory("neo4j"))
+			.setConfig(BoltConnector.enabled, true)
+			.setConfig(BoltConnector.listen_address, new SocketAddress("localhost", 7687))
+			.build();
+		System.out.println("Neo4j (embedded) started.");
+	}
+}

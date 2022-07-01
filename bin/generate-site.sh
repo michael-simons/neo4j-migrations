3e25207ed273ff5c4f917a3262be5081236471fd
Michael Simons <michael@simons.ac>
2022-07-01 07:52:13 +0200

    build: Completely exclude asciidoc extension from site plugin.
    Excluding them via -pl -:neo4j-migrations-formats-adoc leads to another
    error and doesn't work.

--- bin/generate-site.sh
+++ bin/generate-site.sh
@@ -15,7 +15,12 @@ cd $DIR/..
 # For whatever reasons maven-project-info-reports-plugin:3.1.2:dependencies goes nuts on that one
 # it's a pom project, there will be no classes, but apparently the plugin thinks different
 mkdir -p neo4j-migrations-spring-boot-starter-parent/neo4j-migrations-spring-boot-starter/target/classes
+# This fails for some weird Ruby / JRuby errors on GitHub; test results modified as they depend from the ext as well.
+sed -i.bak -e '/<module>extensions\/neo4j-migrations-formats-adoc<\/module>/d' -e '/<module>neo4j-migrations-test-results<\/module>/d' pom.xml
+# Now we can go
 ./mvnw --no-transfer-progress -q compile site:site site:stage
+# Restore things
+mv pom.xml.bak pom.xml
 
 # Copy the site
 rm -rf $TARGET_FOLDER/site &&

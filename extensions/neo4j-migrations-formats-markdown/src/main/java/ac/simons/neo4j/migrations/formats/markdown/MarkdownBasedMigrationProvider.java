a1b2a23cecc778337a095c291faf154144b22b6c
Gerrit Meier <meistermeier@gmail.com>
2022-07-27 15:25:15 +0200

    Move module to correct coordinates.

--- extensions/neo4j-migrations-formats-markdown/src/main/java/ac/simons/neo4j/migrations/formats/markdown/MarkdownBasedMigrationProvider.java
+++ extensions/neo4j-migrations-formats-markdown/src/main/java/ac/simons/neo4j/migrations/formats/markdown/MarkdownBasedMigrationProvider.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package ac.simons.neo4j.migrations.formats.adoc;
+package ac.simons.neo4j.migrations.formats.markdown;
 
 import ac.simons.neo4j.migrations.core.Defaults;
 import ac.simons.neo4j.migrations.core.Migration;
--- extensions/neo4j-migrations-formats-markdown/src/test/java/ac/simons/neo4j/migrations/formats/markdown/MarkdownBasedMigrationProviderTest.java
+++ extensions/neo4j-migrations-formats-markdown/src/test/java/ac/simons/neo4j/migrations/formats/markdown/MarkdownBasedMigrationProviderTest.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package ac.simons.neo4j.migrations.formats.adoc;
+package ac.simons.neo4j.migrations.formats.markdown;
 
 import static org.assertj.core.api.Assertions.assertThat;
 

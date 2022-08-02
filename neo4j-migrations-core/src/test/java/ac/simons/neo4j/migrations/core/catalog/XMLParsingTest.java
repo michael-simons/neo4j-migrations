a99b33fa86bd3fce268a96e72e020627217dca08
Michael Simons <michael@simons.ac>
2022-08-02 16:02:09 +0200

    fix: Add missing type `property` to allowed index types.

--- neo4j-migrations-core/src/test/java/ac/simons/neo4j/migrations/core/catalog/XMLParsingTest.java
+++ neo4j-migrations-core/src/test/java/ac/simons/neo4j/migrations/core/catalog/XMLParsingTest.java
@@ -19,6 +19,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 import ac.simons.neo4j.migrations.core.MigrationsException;
+import ac.simons.neo4j.migrations.core.internal.ThrowingErrorHandler;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
@@ -27,9 +28,13 @@ import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.util.Objects;
 
+import javax.xml.XMLConstants;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
 
 import org.junit.jupiter.api.Test;
 import org.w3c.dom.Document;
@@ -48,9 +53,31 @@ class XMLParsingTest {
 		return value;
 	});
 
-	private static Catalog load(URL url) {
+	private static final ThreadLocal<DocumentBuilderFactory> VALIDATING_DOCUMENT_BUILDER_FACTORY = ThreadLocal.withInitial(() -> {
+
+		SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+		DocumentBuilderFactory value = DocumentBuilderFactory.newInstance();
+		value.setExpandEntityReferences(false);
+		value.setNamespaceAware(true);
+		try {
+			Schema schema = schemaFactory.newSchema(new StreamSource(
+				XMLParsingTest.class.getResourceAsStream("/ac/simons/neo4j/migrations/core/migration.xsd")));
+			value.setSchema(schema);
+		} catch (SAXException e) {
+			throw new RuntimeException(e);
+		}
+		return value;
+	});
+
+	private static Catalog load(URL url, boolean validate) {
 		try (InputStream source = url.openStream()) {
-			DocumentBuilder documentBuilder = DOCUMENT_BUILDER_FACTORY.get().newDocumentBuilder();
+			DocumentBuilder documentBuilder;
+			if (validate) {
+				documentBuilder = VALIDATING_DOCUMENT_BUILDER_FACTORY.get().newDocumentBuilder();
+				documentBuilder.setErrorHandler(new ThrowingErrorHandler());
+			} else {
+				documentBuilder = DOCUMENT_BUILDER_FACTORY.get().newDocumentBuilder();
+			}
 			return Catalog.of(documentBuilder.parse(source));
 		} catch (SAXException | IOException | ParserConfigurationException e) {
 			throw new MigrationsException("Could not parse the given document.", e);
@@ -59,7 +86,7 @@ class XMLParsingTest {
 
 	@Test
 	void shouldFindAllConstraintsAndIndexes() {
-		Catalog catalog = load(XMLParsingTest.class.getResource("/catalogbased/parsing/full-example.xml"));
+		Catalog catalog = load(XMLParsingTest.class.getResource("/catalogbased/parsing/full-example.xml"), true);
 		assertThat(catalog.getItems().stream().filter(Constraint.class::isInstance)
 			.map(Constraint.class::cast))
 			.extracting(Constraint::getName)
@@ -68,12 +95,12 @@ class XMLParsingTest {
 		assertThat(catalog.getItems().stream().filter(Index.class::isInstance)
 			.map(Index.class::cast))
 			.extracting(Index::getName)
-			.containsExactlyInAnyOrder(Name.of("reads_index"), Name.of("title_index"));
+			.containsExactlyInAnyOrder(Name.of("reads_index"), Name.of("title_index"), Name.of("metrics"));
 
 		assertThat(catalog.getItems().stream().filter(Index.class::isInstance)
 			.map(Index.class::cast))
 			.extracting(Index::getType)
-			.containsExactlyInAnyOrder(Index.Type.FULLTEXT, Index.Type.PROPERTY);
+			.containsExactlyInAnyOrder(Index.Type.FULLTEXT, Index.Type.PROPERTY, Index.Type.PROPERTY);
 	}
 
 	@Test
@@ -81,7 +108,7 @@ class XMLParsingTest {
 
 		URL resource = XMLParsingTest.class.getResource("/catalogbased/parsing/invalid_constraint.xml");
 		Objects.requireNonNull(resource);
-		assertThatIllegalArgumentException().isThrownBy(() -> load(resource));
+		assertThatIllegalArgumentException().isThrownBy(() -> load(resource, false));
 	}
 
 	@Test

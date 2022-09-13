d92cc5388ef4234f6bb682120da3ec52577add1b
Michael J. Simons <michael@simons.ac>
2022-09-13 15:33:46 +0200

    fix: Add several missing JMS requirements.
    This add missing requirements for `java.xml(.crypto)` as well as for the
    Cypher-DSL schema name support. Not having these will most likely cause
    issues on the module path.

--- neo4j-migrations-core/src/main/java11/module-info.java
+++ neo4j-migrations-core/src/main/java11/module-info.java
@@ -21,6 +21,9 @@ import ac.simons.neo4j.migrations.core.ResourceBasedMigrationProvider;
 module ac.simons.neo4j.migrations.core {
 
 	requires io.github.classgraph;
+	requires java.xml;
+	requires java.xml.crypto;
+	requires org.neo4j.cypherdsl.support.schema_name;
 
 	requires transitive java.logging;
 

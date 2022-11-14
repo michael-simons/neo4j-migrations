13d7d451c326a245769095ab37e79277836fd99c
Michael Simons <michael@simons.ac>
2022-11-14 12:09:06 +0100

    build: Improve Antora version approach and module name. (#738)
    Co-authored-by: Guillaume Grossetie <ggrossetie@yuzutech.fr>

---
+++ bin/bump-version.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+DIR="$(dirname "$(realpath "$0")")"
+
+NUM_DOTS=$(echo "${1}" | awk -F. '{ print NF - 1 }')
+
+if [[ "${1}" == *"SNAPSHOT" ]]; then
+  if [[ $NUM_DOTS -lt 2 ]]; then
+    echo "Version must be either an plain version or a full x.y.z-SNAPSHOT version"
+    exit
+  fi
+  VERSION="${1}"
+else
+  if [[ $NUM_DOTS -eq 0 ]]; then
+    VERSION="${1}.0.0-SNAPSHOT"
+  elif [[ $NUM_DOTS -eq 1 ]]; then
+    VERSION="${1}.0-SNAPSHOT"
+  else
+    VERSION="${1}-SNAPSHOT"
+  fi
+fi
+
+# shellcheck disable=SC2001
+MINOR_VERSION=$(echo "${VERSION}" | sed 's/\.[^\.]*$//')
+
+# Replace version
+# ...in Antora playbook
+sed -i .bak \
+  -e 's/\(version: &the-version\) \(.*\)/\1 "'"${MINOR_VERSION}"'"/g' \
+  -e 's/\(prerelease:\) .*/\1 "SNAPSHOT"/g' \
+  "$DIR"/../docs/antora.yml
+rm "$DIR"/../docs/antora.yml.bak
+# ...in Maven build descriptor
+./mvnw -f "$DIR"/../pom.xml versions:set -DgenerateBackupPoms=false -DnewVersion="${VERSION}"
--- bin/prepare-release.sh
+++ bin/prepare-release.sh
@@ -3,14 +3,16 @@
 set -euo pipefail
 DIR="$(dirname "$(realpath "$0")")"
 
-sed -i .bak 's/\(:version:\) \(.*\)/\1 '"${1}"'/g' $DIR/../README.adoc
-rm $DIR/../README.adoc.bak
+sed -i .bak 's/\(:version:\) \(.*\)/\1 '"${1}"'/g' "$DIR"/../README.adoc
+rm "$DIR"/../README.adoc.bak
 
-sed -i .bak 's/\(version: &the-version\) \(.*\)/\1 '"${1}"'/g' $DIR/../docs/antora.yml
-rm $DIR/../docs/antora.yml.bak
-
-sed -i .bak '/prerelease: "true"/d' $DIR/../docs/antora.yml
-rm $DIR/../docs/antora.yml.bak
+# shellcheck disable=SC2001
+MINOR_VERSION=$(echo "${1}" | sed 's/\.[^\.]*$//')
+sed -i .bak \
+  -e 's/\(version: &the-version\) \(.*\)/\1 "'"${MINOR_VERSION}"'"/g' \
+  -e 's/\(prerelease:\) .*/\1 "false"/g' \
+  "$DIR"/../docs/antora.yml
+rm "$DIR"/../docs/antora.yml.bak
 
 if test -n "${2-}"; then
   DRYRUN=$2
@@ -19,6 +21,6 @@ else
 fi
 
 if [ "$DRYRUN" != "true" ]; then
-  git add $DIR/../README.adoc
+  git add "$DIR"/../README.adoc
   git commit -m "[maven-release-plugin] update README.adoc"
 fi

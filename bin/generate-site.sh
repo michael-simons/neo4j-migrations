a3d01eef7a85428fe368add73b639fbe7428a5f2
Michael J. Simons <michael@simons.ac>
2023-03-01 00:23:30 +0100

    Polish.
    nocl

--- bin/generate-site.sh
+++ bin/generate-site.sh
@@ -12,7 +12,10 @@ cd "$DIR"/..
 # Generate the documentation
 ./mvnw --no-transfer-progress asciidoctor:process-asciidoc@generate-docs -pl :neo4j-migrations-parent -Dproject.build.docs="$TARGET_FOLDER" -Dproject.build.docs.branch="$SOURCE_BRANCH"
 # Generate the site
-./mvnw --no-transfer-progress -q -Dfast -Drelease -Dmaven.javadoc.skip=false package site:site site:stage
+# The asciidoc extensions fails for some weird Ruby / JRuby errors on GitHub; test results modified as they depend on that module as well.
+sed -i.bak -e '/<module>extensions\/neo4j-migrations-formats-adoc<\/module>/d' -e '/<module>neo4j-migrations-test-results<\/module>/d' pom.xml
+./mvnw --no-transfer-progress -q -Dfast -Dmaven.javadoc.skip=false package site:site site:stage
+mv pom.xml.bak pom.xml
 
 # Copy the site
 rm -rf "$TARGET_FOLDER"/site &&

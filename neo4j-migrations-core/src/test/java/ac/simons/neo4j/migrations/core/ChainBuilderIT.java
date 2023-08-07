78fd7d441d79be07be983c28e826e07eb23a12f3
Michael Simons <michael@simons.ac>
2023-08-07 13:44:01 +0200

    build: Use Neo4j 5.10 by default for testing.

--- neo4j-migrations-core/src/test/java/ac/simons/neo4j/migrations/core/ChainBuilderIT.java
+++ neo4j-migrations-core/src/test/java/ac/simons/neo4j/migrations/core/ChainBuilderIT.java
@@ -45,7 +45,7 @@ class ChainBuilderIT extends TestBase {
 			.buildChain(context, new DiscoveryService().findMigrations(context));
 
 		assertThat(migrationChain.getServerAddress()).isEqualTo(getServerAddress());
-		assertThat(migrationChain.getServerVersion()).matches("Neo4j/(?:\\d\\.){2}\\d+");
+		assertThat(migrationChain.getServerVersion()).matches("Neo4j/\\d\\.\\d{1,2}\\.\\d+");
 		assertThat(migrationChain.getServerEdition()).isEqualTo("Community");
 		assertThat(migrationChain.getUsername()).isEqualTo("neo4j");
 		assertThat(migrationChain.getOptionalDatabaseName()).hasValue("neo4j");

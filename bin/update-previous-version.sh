613a0839517b1dbf04e7590b76c44b0de3b85239
Michael Simons <michael@simons.ac>
2024-03-05 18:51:46 +0100

    build: Improve version updater script.

--- bin/update-previous-version.sh
+++ bin/update-previous-version.sh
@@ -3,5 +3,5 @@
 set -euo pipefail
 DIR="$(dirname "$(realpath "$0")")"
 
-NEW_OLD_VERSION=$(sed -n 's/project\.rel\.eu\.michael-simons\.neo4j\\:neo4j-migrations-parent=\(.*\)/\1/p' $DIR/../release.properties)
-$DIR/../mvnw versions:set-property -DgenerateBackupPoms=false -Dproperty=neo4j-migrations.previous.version -DnewVersion=$NEW_OLD_VERSION -pl :neo4j-migrations-parent
+NEW_OLD_VERSION=$(sed -n 's/project\.rel\.eu\.michael-simons\.neo4j\\:neo4j-migrations-parent=\(.*\)/\1/p' "$DIR"/../release.properties)
+"$DIR"/../mvnw versions:set-property -DgenerateBackupPoms=false -Dproperty=neo4j-migrations.previous.version -DnewVersion="$NEW_OLD_VERSION" -pl :neo4j-migrations-parent

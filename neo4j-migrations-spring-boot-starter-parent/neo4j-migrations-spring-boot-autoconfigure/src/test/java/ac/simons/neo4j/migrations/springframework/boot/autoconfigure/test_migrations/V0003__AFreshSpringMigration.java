4dce77843de0de6f2c3969b62e29234439151be7
Michael Simons <michael@simons.ac>
2024-07-26 13:37:50 +0200

    feat: Add support for using Spring Beans as Java based migrations. (#1395)
    Closes #1393.

---
+++ neo4j-migrations-spring-boot-starter-parent/neo4j-migrations-spring-boot-autoconfigure/src/test/java/ac/simons/neo4j/migrations/springframework/boot/autoconfigure/test_migrations/V0003__AFreshSpringMigration.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2020-2024 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package ac.simons.neo4j.migrations.springframework.boot.autoconfigure.test_migrations;
+
+import java.util.Map;
+import java.util.Objects;
+
+import org.neo4j.driver.Session;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+
+import ac.simons.neo4j.migrations.core.JavaBasedMigration;
+import ac.simons.neo4j.migrations.core.MigrationContext;
+
+/**
+ * Example for a Spring based Java migration.
+ */
+@Component
+public class V0003__AFreshSpringMigration implements JavaBasedMigration {
+
+	private final Environment environment;
+
+	public V0003__AFreshSpringMigration(Environment environment) {
+		this.environment = environment;
+	}
+
+	@Override
+	public void apply(MigrationContext context) {
+		try (Session session = context.getSession()) {
+
+			var property = Objects.requireNonNull(environment.getProperty("spring.application.name"));
+			session.run("CREATE (onA:WALL{graffito: 'Spring is in the air', appName: $appName})", Map.of("appName", property));
+		}
+	}
+}

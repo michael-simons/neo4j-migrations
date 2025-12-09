5e3e5f57ab0f1c768ff3f61b57e776bc0ca7bff2
Michael Simons <michael@simons.ac>
2025-12-09 18:10:14 +0100

    feat: Add wrapper for `@Index` so that multiple indexes can be defined on a type. (#1849)
    Closes #1842.

---
+++ extensions/neo4j-migrations-annotation-processing/processor/src/test/java/ac/simons/neo4j/migrations/annotations/proc/catalog/valid/CoffeeBeanMultipleIndexes.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2020-2025 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package ac.simons.neo4j.migrations.annotations.proc.catalog.valid;
+
+import java.util.UUID;
+
+import ac.simons.neo4j.migrations.annotations.catalog.Index;
+
+/**
+ * @author Michael J. Simons
+ */
+@Index(properties = { "uuid" })
+@Index(properties = { "a", "b", "c" })
+public class CoffeeBeanMultipleIndexes {
+
+	public UUID uuid;
+
+	public String a;
+
+	public String b;
+
+	public String d;
+
+}
